98cc9386b1d39ab9ae44238fd7ce54face54be9a
Eric Li <efilm0@gmail.com>
2013-10-27 00:47:17 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/* global require */
+'use strict';
+
+require.config({
+	// The shim config allows us to configure dependencies for
+	// scripts that do not call define() to register a module
+	shim: {
+		jquery: {
+			exports: '$'
+		},
+		underscore: {
+			exports: '_',
+			deps: ['jquery']
+		}
+	},
+	paths: {
+		jquery: 'libs/jquery/jquery-1.10.2.min',
+		underscore: 'libs/underscore/underscore-min',
+		text: 'libs/require/text'
+	}
+});
+
+require([
+	'jquery',
+], function ($, test) {
+	$(document).ready(function() {
+		console.log('it works!');
+	});
+});
