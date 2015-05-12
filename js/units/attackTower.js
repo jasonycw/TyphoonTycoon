0374399e88ad370c9559f16f912d9b26d23aec62
Chui, Hin Wah <hinwahch@uk.ibm.com>
2015-05-12 17:58:21 +0100

    Gave earthquake a new look,
    Giving player clear info about which building is going to lose.
    also:
    - fixed readme being covered by game
    - fixed buildeffect/ripple to count realtime

--- js/units/attackTower.js
+++ js/units/attackTower.js
@@ -4,11 +4,14 @@ define([
 	'models/laser',
 	'config',
 	'models/buildEffect'
-], function(Sound, Tower, Laser, Config, BuildEffect) {
+], function(Sound, Tower, Laser, Config, BuildEffect)
+{
 	// Create Tower Object and its constructor
-	function AttackTower(game, startX, startY, spriteSrc) {
+	function AttackTower(game, startX, startY, spriteSrc)
+	{
 		//call super constructor.
-		Tower.call(this, startX, startY, spriteSrc, game, "AttackTower");
+		Tower.call(this, startX, startY, spriteSrc, game,
+			"AttackTower");
 		this.coolDownTime = 15;
 		this.coolDownCounter = 0;
 		this.sound = new Sound('attackTowerSound');
@@ -20,37 +23,55 @@ define([
 
 
 	// tick event handler
-	AttackTower.prototype.tick = function(dt) { // override
-		if (this.coolDownCounter <= 0) {
+	AttackTower.prototype.tick = function(dt)
+	{ // override
+		if (this.coolDownCounter <= 0)
+		{
 
 			// calculate power based on structures built
 			var _range = this.config.range;
 			var _attackDamage = this.config.attackDamage;
-			if (this.game.isBuilt('University')) {
-				_range += Config.University.attackTowerRangeIncrease*this.game.numberOfBuilding('University');
-				_attackDamage += Config.University.attackTowerAttackIncrease*this.game.numberOfBuilding('University');
+			if (this.game.isBuilt('University'))
+			{
+				_range += Config.University.attackTowerRangeIncrease *
+					this.game.numberOfBuilding('University');
+				_attackDamage += Config.University.attackTowerAttackIncrease *
+					this.game.numberOfBuilding('University');
 			}
-			if (this.game.isBuilt('ResearchCenter')) {
-				_range += Config.ResearchCenter.attackTowerRangeIncrease*this.game.numberOfBuilding('ResearchCenter');
-				_attackDamage += Config.ResearchCenter.attackTowerAttackIncrease*this.game.numberOfBuilding('ResearchCenter');
+			if (this.game.isBuilt('ResearchCenter'))
+			{
+				_range += Config.ResearchCenter.attackTowerRangeIncrease *
+					this.game.numberOfBuilding('ResearchCenter');
+				_attackDamage += Config.ResearchCenter.attackTowerAttackIncrease *
+					this.game.numberOfBuilding('ResearchCenter');
 			}
 
 			// actual attack
 			var nearestEnemy = this.findNearestEnemyWithin(_range);
 
-			if (nearestEnemy) {
-				if (nearestEnemy.distance <= _range && this.game.getAvailablePower() > 0) {
+			if (nearestEnemy)
+			{
+				if (nearestEnemy.distance <= _range &&
+					this.game.getAvailablePower() > 0)
+				{
 					var enemyWidth = nearestEnemy.target.sprite.width;
-					var aimX = nearestEnemy.target.x - enemyWidth / 8 + Math.random() * enemyWidth / 4;
-					var aimY = nearestEnemy.target.y - enemyWidth / 8 + Math.random() * enemyWidth / 4;
-					var laser = new Laser(this.x, this.y, aimX, aimY, "red", 10, 3);
+					var aimX = nearestEnemy.target.x - enemyWidth /
+						8 + Math.random() * enemyWidth / 4;
+					var aimY = nearestEnemy.target.y - enemyWidth /
+						8 + Math.random() * enemyWidth / 4;
+					var laser = new Laser(this.x, this.y, aimX,
+						aimY, "red", 10, 3);
 					this.sound.play('laser');
-					var buildEffect = new BuildEffect(aimX, aimY, "red", 15, 7, 1);
+					var buildEffect = new BuildEffect(aimX, aimY,
+						"red", 0.4, 7, 1);
+
 					nearestEnemy.target.damage(_attackDamage);
 					this.coolDownCounter = this.coolDownTime;
 				}
 			}
-		} else {
+		}
+		else
+		{
 			this.coolDownCounter--;
 		}
 	};
