ce4ab71598d38c0b28e375ce3dc4d99d45ac89ae
Jason Yu <jasonycw1992@gmail.com>
2026-04-25 20:50:19 +0800

    Add responsive mobile input and layout support

--- js/models/ui.js
+++ js/models/ui.js
@@ -82,6 +82,11 @@ define([
 			bgm.currentTime = 0;
 		},
 		bindButtonTooltip: function() {
+			var isTouchDevice = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0);
+			if (isTouchDevice) {
+				return;
+			}
+
 			var btnIds = ['btn-power-plant', 'btn-laser-tower', 'btn-freeze-tower', 'btn-repel-tower', 'btn-nuclear-plant', 'btn-university', 'btn-research-center', 'btn-cheung-kong'];
 			var configIds = ['PowerPlant', 'AttackTower', 'FreezeTower', 'RepelTower', 'NuclearPlant', 'University', 'ResearchCenter', 'CheungKong'];
 
@@ -160,7 +165,10 @@ define([
 		},
 		bindCanvasMouseMoveEvent: function() {
 			var that = this;
-			this.$canvas.mousemove(function(e) {
+			this.$canvas.on('pointermove', function(e) {
+				if (e.pointerType && e.pointerType !== 'mouse') {
+					return;
+				}
 				if (!that.activatedMode) {
 					that.$canvas.css('cursor', 'default');
 					return;
@@ -277,7 +285,8 @@ define([
 		},
 		bindCanvasClickEvent: function() {
 			var that = this;
-			$('#game-canvas').click(function(event) {
+			$('#game-canvas').on('pointerdown', function(event) {
+				event.preventDefault();
 				if(that.activatedMode == null) return;
 
 				var structureClass = that.structureClassMap[that.activatedMode];
